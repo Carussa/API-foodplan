89584f7daa5c2e221ed88e280953600905a8c34a
Carussa <106354400+Carussa@users.noreply.github.com>
2022-05-27 01:42:32 +0200

    Add files via upload

---
+++ recetas.php
@@ -0,0 +1,66 @@
+<?php
+    class Receta{
+
+        // Connection
+        private $connection;
+
+        // Table
+        private $db_table_1 = "recetas";
+        private $db_table_2 = "ingredientes_receta";
+        private $db_table_3 = "ingredientes";
+
+        // Columns
+        public $id;
+        public $titulo;
+        public $descripcion;
+        public $imagen;
+        public $ingredientes;
+
+        // Db connection
+        public function __construct($db){
+            $this->connection = $db;
+        }
+
+        // GET ALL
+        public function getRecetas(){
+            $consulta = "SELECT idreceta, titulo, imagen FROM " . $this->db_table_1 . "";
+            $resultado = mysqli_query($this->connection, $consulta);
+
+            if (mysqli_num_rows($resultado) > 0) {
+                // Devuelve el array con todos los datos de todos los artículos
+                return $resultado;
+            }
+        }
+
+        public function getReceta(){
+            $consulta = "SELECT titulo, descripcion, imagen FROM " . $this->db_table_1 . " WHERE idreceta = " . $this->id . "";
+            $resultado = mysqli_query($this->connection, $consulta);
+
+            if (mysqli_num_rows($resultado) == 1) {
+
+                $receta = mysqli_fetch_assoc($resultado);
+                $this->titulo = $receta['titulo'];
+                $this->descripcion = $receta['descripcion'];
+                $this->imagen = $receta['imagen'];
+                $this->getIngredientesReceta();
+            }
+        }        
+
+
+        public function getIngredientesReceta(){
+            $consulta = "SELECT nombre FROM " . $this->db_table_3 . " a INNER JOIN " . $this->db_table_2 . " b WHERE b.idreceta = " . $this->id ." and a.idingrediente = b.idingrediente";
+           
+            $resultado = mysqli_query($this->connection, $consulta); 
+
+            if (mysqli_num_rows($resultado) > 0) {
+                $this->ingredientes = [];
+                while ($fila = mysqli_fetch_assoc($resultado)){
+                    extract($fila);
+                    array_push($this->ingredientes, $nombre);
+                }
+            }
+        }
+
+    }
+?>
+
---
+++ single_read.php
@@ -0,0 +1,39 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+    include_once '../config/database.php';
+    include_once '../class/recetas.php';
+
+    $database = new Database();
+    $db = $database->getConnection();
+
+    $receta = new Receta($db);
+
+    $receta->id = isset($_GET['id']) ? $_GET['id'] : die();
+  
+    $receta->getReceta();
+
+    if($receta->titulo != null){
+        // create array
+        $receta_detalles = array(
+            "id" => $receta->id,
+            "titulo" => $receta->titulo,
+            "descripcion" => $receta->descripcion,
+            "imagen" => $receta->imagen,
+        );
+      
+        if ($receta->ingredientes != null) $receta_detalles["ingredientes"] = $receta->ingredientes;
+
+        http_response_code(200);
+        echo json_encode($receta_detalles);
+    }
+      
+    else{
+        http_response_code(404);
+        echo json_encode("Employee not found.");
+    }
+?>
